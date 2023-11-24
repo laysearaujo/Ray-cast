85ec7ebba010fe51abf9f7d7fa630164ce8e3851
Eduardo <eduardoaqz@gmail.com>
2023-11-23 22:19:15 -0300

    add intersect

--- sirius/sphere.py
+++ sirius/sphere.py
@@ -1,6 +1,7 @@
 from .vector import Vector
 from .color import Color
 from .ray import Ray
+import math
 
 class Sphere:
     def __init__(self, center: Vector, radius: float, color: Color):
@@ -8,5 +9,32 @@ class Sphere:
         self.radius = radius
         self.color = color
 
-    def intersectRay(ray: Ray):
-        return ray
+    def normalAt(self, point: Vector):
+        normal_vector = point.sub(self.center)
+        return normal_vector.normalize()
+
+    def intersectRay(self, ray: Ray):
+        oc = ray.point.sub(self.center)
+
+        a = ray.direction.dot(ray.direction)
+        b = 2.0 * oc.dot(ray.direction)
+        c = oc.dot(oc) - self.radius * self.radius
+
+        delta = b * b - 4 * a * c
+
+        # Se delta negativo
+        if delta < 0:
+            return {
+                "color": None,
+                "distance": None,
+                "normal": None
+            }
+
+        # Calcula as duas soluções da equação quadrática
+        t = (-b - math.sqrt(delta)) / (2 * a)
+
+        return {
+                "color": self.color,
+                "distance": t,
+                # "normal": self.normalAt()
+            }
