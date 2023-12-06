9f45661aa14600b70c07152751f3cd3a5e2f87f7
“laysearaujo” <lrsa@cin.ufpe.br>
2023-12-06 17:54:16 -0300

    clear pygame

--- main.py
+++ main.py
@@ -10,16 +10,11 @@ from sirius.ray import Ray
 QUIT = 'q'
 
 def main():
-    # pygame.init()
     width, height = 800, 600
     location = Vector(0, 0, 0)
     focus = Vector(0, 0, 1)
     v_up = Vector(0, 1, 0)
     distance = 40
-    # screen = pygame.display.set_mode((width, height))
-    # pygame.display.set_caption('Ray Tracer')
-
-    # clock = pygame.time.Clock()
 
     spheres = [
         Sphere(Vector(5, 5, 5), 5, Color(255, 0, 0)),  # Esfera vermelha
@@ -50,28 +45,9 @@ def main():
     instersect3_color, instersect3_distance, _ = planes[0].intersect(ray).values()
     print(f'Intersecção da esfera vermelha com o plano - distancia: {instersect3_distance}, cor: {instersect3_color}')
     
-
-    # running = True
-    # while running:
-    #     for event in pygame.event.get():
-    #         if event.type == QUIT:
-    #             running = False
-
     matrix = camera.take(scene)
         
-    # Criando uma surface a partir da matriz
-    # surface = pygame.surfarray.make_surface(matrix)
-
-    # Redimensionando a surface para o tamanho da janela
-    # surface = pygame.transform.scale(surface, (width, height))
-
-    # Blit da surface na tela do Pygame
-    # screen.blit(surface, (0, 0))
-
-    # pygame.display.flip()
-    # clock.tick(60)
-
-    # pygame.quit()
+    print(matrix)
 
 if __name__ == "__main__":
     main()
