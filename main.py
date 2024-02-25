7cfc6f634bb652787616c49222b8ddea1a6278da
laysearaujo <lrsa@cin.ufpe.br>
2024-02-25 13:46:16 -0300

    main with sphere

--- main.py
+++ main.py
@@ -5,80 +5,58 @@ import numpy as np
 from src.graphic.camera import Camera
 from src.graphic.scene import Scene
 from src.geometry.vector import Vector
-from src.geometry.triangularMesh import TriangularMesh, Triangle
+from src.geometry.triangularMesh import TriangularMesh
+from src.entity.triangle import Triangle
 from src.utils.loaders import Loaders
+from src.entity.sphere import Sphere
+from src.entity.plane import Plane
+from src.graphic.color import Color
 
 def main():
-    # Carregar vértices e triângulos do arquivo OBJ
-    vertices, triangles = Loaders.importObjFile('src/assets/humanoid.obj')
-
-    triangles_rotated = []
-
-    # Aplicar transformação de rotação de 90 graus no eixo X
-    rotated_vertices = [vertex.rotateY(math.pi / 2) for vertex in vertices]
-
-    for triangle in triangles:
-        triangle_rotated_vertices = [vertex.rotateY(math.pi / 2) for vertex in triangle.vertices]
-        triangles_rotated.append(
-            Triangle(triangle_rotated_vertices[0], 
-            triangle_rotated_vertices[1], 
-            triangle_rotated_vertices[2], 
-            triangle.color ))
-            
-    # Ajustar o tamanho da janela
-    width, height = 400, 300
+    width, height = 800, 600
     v_up = Vector(0, 1, 0)
 
-    # Calcular o centro dos vértices para posicionar a câmera
-    center = Vector(0, 0, 0)
-    for vertex in vertices:
-        center = center.add(vertex)
-    center = center.multByScalar(1 / len(vertices))
+    # Ajustando os parâmetros da câmera para melhor visualização
+    location = Vector(0, 0, -25)  # Posição da câmera mais recuada
+    focus = Vector(0, 0, 0)       # Alterando a direção do foco para o centro da cena
+    distance = 70                 # Reduzindo a distância da câmera
 
-    # Ajustar a posição e a distância da câmera para enquadrar o modelo
-    location = Vector(center.x, center.y, center.z + 50)
-    focus = center
-    distance = 10
+    spheres = [
+        Sphere(Vector(-10, 0, 0), 5, Color(255, 0, 0)),     # Esfera vermelha à esquerda
+        Sphere(Vector(0, 0, 0), 5, Color(0, 255, 0)),      # Esfera verde no centro
+        Sphere(Vector(10, 0, 0), 5, Color(0, 0, 255))      # Esfera azul à direita
+    ]
 
-    # Criar a malha triangular e adicionar os triângulos a ela
-    mesh = TriangularMesh(vertices, triangles)
-    mesh_rotated = TriangularMesh(rotated_vertices, triangles_rotated)
+    planes = [
+        Plane(Vector(0, -15, 0), Vector(0, 1, 0), Color(200, 200, 200))  # Plano abaixo das esferas
+    ]
 
-    for i, vertex in enumerate(mesh.vertices):
-        mesh_rotated.vertices[i] = vertex.rotateY(math.pi / 2)
-
-    # Criar a cena e adicionar as malhas
     scene = Scene()
-    scene.addMesh(mesh)
-    scene_rotated = Scene()
-    scene_rotated.addMesh(mesh_rotated)
 
-    # Configurar a câmera e gerar as imagens para ambas as cenas
+    for sphere in spheres:
+        scene.addSphere(sphere)
+    
+    for plane in planes:
+        scene.addPlane(plane)
+
     camera = Camera(location, focus, v_up, distance, width, height)
-    matrix = camera.take(scene)
-    matrix_rotated = camera.take(scene_rotated)
 
-    # Converter as matrizes de cores para imagens OpenCV (original e rotacionada)
+    # # Teste de intersecção de raio
+    # ray = Ray(Vector(0,0,0), Vector(1,1,0))
+    # instersect1_color, instersect1_distance, _ = spheres[0].intersect(ray).values()
+
+    matrix = camera.take(scene)
+
+    # Convertendo a matriz de cores para uma imagem OpenCV
     img = np.zeros((height, width, 3), dtype=np.uint8)
-    img_transformed = np.zeros((height, width, 3), dtype=np.uint8)
 
     for y in range(height):
         for x in range(width):
             color = matrix[y][x]
             img[y][x] = [int(color.r), int(color.g), int(color.b)]
 
-            color_rotated = matrix_rotated[y][x]
-            img_transformed[y][x] = [int(color_rotated.r), int(color_rotated.g), int(color_rotated.b)]
-
-           # print(img == img_transformed)
-
-    # Criar a imagem combinada (original e rotacionada)
-    combined_img = np.zeros((height, 2 * width, 3), dtype=np.uint8)
-    combined_img[:height, :width, :] = img
-    combined_img[:height, width:2 * width, :] = img_transformed
-
-    # Exibir as duas imagens lado a lado
-    cv2.imshow('Imagem Original vs. Rotacionada', combined_img)
+    # Exibir a imagem usando OpenCV
+    cv2.imshow('Render', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
